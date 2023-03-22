97c15a34870c98513ba9cc5de20195ae745317c5
Ecem Gokdogan <ecemgo@hotmail.com>
2023-03-22 19:07:54 +0200

    margin values & regex updated

--- pod-request-access-landing-page/main.js
+++ pod-request-access-landing-page/main.js
@@ -1,22 +1,18 @@
 const emailId = document.getElementById("email-id");
 const error = document.getElementById("error");
 const remark = document.getElementById("remark");
-const mailRegex =
-  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const mailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
 //! check whether email address is valid or not
 function checker() {
   if (emailId.value.match(mailRegex)) {
     remark.innerHTML = `<i class="fa-solid fa-circle-check"></i><span>Your email is now valid!</span>`;
-    remark.style.margin = "0 0 3rem 0";
     error.style.display = "none";
   } else if (emailId.value == "") {
-    remark.style.display = "none";
     error.style.display = "none";
-    emailId.style.margin = "0 0 4.5rem 0";
+    remark.style.display = "none";
   } else {
     error.style.display = "block";
-    emailId.style.margin = "0 0 0 0";
   }
 }
 
