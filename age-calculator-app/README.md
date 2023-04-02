d0700dc0722ad3d6ab31f1302f16834575caf5e6
Ecem Gokdogan <ecemgo@hotmail.com>
2023-04-02 17:49:23 +0300

    updated the name of the folder

---
+++ age-calculator-app/README.md
@@ -0,0 +1,59 @@
+# <img src="https://user-images.githubusercontent.com/13468728/222973742-9133bdb5-61f0-4f53-8b08-bb3c349e2056.png" title="Frontend Mentor" alt="Frontend Mentor" width="50" height="50"/> Frontend Mentor - Age Calculator App Solution
+
+This is a solution to the [Age calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/age-calculator-app-dF9DFFpj-Q). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View an age in years, months, and days after submitting a valid date through the form
+- Receive validation errors if:
+  - Any field is empty when the form is submitted
+  - The day number is not between 1-31
+  - The month number is not between 1-12
+  - The year is in the future
+  - The date is invalid e.g. 31/04/1991 (there are 30 days in April)
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+- **Bonus**: See the age numbers animate to their final number when the form is submitted
+
+### Screenshot
+
+![](./assets/images/screenshot.jpg)
+
+### Links
+
+- [Solution URL](https://your-solution-url.com)
+- [Live Site URL](https://ecemgo-age-calculator.netlify.app/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Animation
+
+### Useful resources
+
+- [Stack Overflow](https://stackoverflow.com/questions/4847996/css-animation-onclick) - It is used for the animated resutls.
+
+## Author
+
+- Website - [ecemgo.com](https://www.ecemgo.com/)
+- Frontend Mentor - [@ecemgo](https://www.frontendmentor.io/profile/ecemgo)
+- Twitter - [@ecemgo](https://twitter.com/ecemgo)
---
+++ age-calculator-app/app.js
@@ -0,0 +1,134 @@
+const dayInput = document.getElementById("day");
+const monthInput = document.getElementById("month");
+const yearInput = document.getElementById("year");
+const dayOutput = document.getElementById("dd");
+const monthOutput = document.getElementById("mm");
+const yearOutput = document.getElementById("yy");
+const form = document.querySelector("form");
+
+const date = new Date();
+let day = date.getDate();
+let month = 1 + date.getMonth();
+let year = date.getFullYear();
+const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+//! check if the input is valid and empty
+function validate() {
+  const inputs = document.querySelectorAll("input");
+  let validator = true;
+  inputs.forEach((i) => {
+    const parent = i.parentElement;
+    if (!i.value) {
+      parent.querySelector("label").style.color = "hsl(0, 100%, 67%)";
+      i.style.borderColor = "hsl(0, 100%, 67%)";
+      parent.querySelector("small").innerText = "This field is required";
+      validator = false;
+    } else if (monthInput.value == 2 && dayInput.value > 28) {
+      if (yearInput.value % 4 != 0) {
+        dayInput.parentElement.querySelector("label").style.color =
+          "hsl(0, 100%, 67%)";
+        dayInput.borderColor = "hsl(0, 100%, 67%)";
+        dayInput.parentElement.querySelector("small").innerText =
+          "Must be a valid day";
+        validator = false;
+      } else if (yearInput.value % 4 == 0 && dayInput.value > 29) {
+        dayInput.parentElement.querySelector("label").style.color =
+          "hsl(0, 100%, 67%)";
+        dayInput.borderColor = "hsl(0, 100%, 67%)";
+        dayInput.parentElement.querySelector("small").innerText =
+          "Must be a valid day";
+        validator = false;
+      } else {
+        dayInput.parentElement.querySelector("label").style.color =
+          "hsl(0, 1%, 44%)";
+        dayInput.borderColor = "hsl(0, 0%, 8%)";
+        dayInput.parentElement.querySelector("small").innerText = "";
+        validator = true;
+      }
+    } else if (
+      monthInput.value == 4 ||
+      monthInput.value == 6 ||
+      monthInput.value == 9 ||
+      monthInput.value == 11
+    ) {
+      if (dayInput.value > 30) {
+        dayInput.parentElement.querySelector("label").style.color =
+          "hsl(0, 100%, 67%)";
+        dayInput.borderColor = "hsl(0, 100%, 67%)";
+        dayInput.parentElement.querySelector("small").innerText =
+          "Must be a valid day";
+        validator = false;
+      } else {
+        dayInput.parentElement.querySelector("label").style.color =
+          "hsl(0, 1%, 44%)";
+        dayInput.borderColor = "hsl(0, 0%, 8%)";
+        dayInput.parentElement.querySelector("small").innerText = "";
+        validator = true;
+      }
+    } else if (dayInput.value > 31 || dayInput.value < 1) {
+      dayInput.parentElement.querySelector("label").style.color =
+        "hsl(0, 100%, 67%)";
+      dayInput.borderColor = "hsl(0, 100%, 67%)";
+      dayInput.parentElement.querySelector("small").innerText =
+        "Must be a valid day";
+      validator = false;
+    } else if (monthInput.value > 12 || monthInput.value < 1) {
+      monthInput.parentElement.querySelector("label").style.color =
+        "hsl(0, 100%, 67%)";
+      monthInput.style.borderBlockColor = "hsl(0, 100%, 67%)";
+      monthInput.parentElement.querySelector("small").innerText =
+        "Must be a valid month";
+      validator = false;
+    } else if (yearInput.value > year) {
+      yearInput.parentElement.querySelector("label").style.color =
+        "hsl(0, 100%, 67%)";
+      yearInput.borderColor = "hsl(0, 100%, 67%)";
+      yearInput.parentElement.querySelector("small").innerText =
+        "Must be in the past";
+      validator = false;
+    } else if (yearInput.value.length > 4 || yearInput.value.length < 4) {
+      yearInput.parentElement.querySelector("label").style.color =
+        "hsl(0, 100%, 67%)";
+      yearInput.borderColor = "hsl(0, 100%, 67%)";
+      yearInput.parentElement.querySelector("small").innerText =
+        "This is invalid year";
+      validator = false;
+    } else {
+      parent.querySelector("label").style.color = "hsl(0, 1%, 44%)";
+      i.style.borderColor = "hsl(0, 0%, 8%)";
+      parent.querySelector("small").innerText = "";
+      validator = true;
+    }
+  });
+  return validator;
+}
+
+//! calculate the age in terms of day, month and year
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (validate()) {
+    if (dayInput.value > day) {
+      day = day + months[month - 1];
+      month = month - 1;
+    }
+    if (monthInput.value > month) {
+      month = month + 12;
+      year = year - 1;
+    }
+
+    const d = day - dayInput.value;
+    const m = month - monthInput.value;
+    const y = year - yearInput.value;
+
+    dayOutput.innerHTML = d;
+    monthOutput.innerHTML = m;
+    yearOutput.innerHTML = y;
+
+    document.getElementById("result").className = "animatedResults";
+  }
+});
+
+//! add the animation to the results
+// function animatedage() {
+//   document.getElementById("result").className = "animatedResults";
+// }
